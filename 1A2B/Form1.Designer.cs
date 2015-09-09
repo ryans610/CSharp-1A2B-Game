c16efd8b530c1ba860ae6e1060995d6818ee6a75
ryans610 <ryans610@gmail.com>
2015-09-09 10:35:30 +0800

    Form File

---
+++ 1A2B/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _1A2B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GuessNumber = new System.Windows.Forms.TextBox();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.NewNumber = new System.Windows.Forms.Button();
+            this.AnswerLabel = new System.Windows.Forms.Label();
+            this.AnswerButton = new System.Windows.Forms.Button();
+            this.ShowBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // GuessNumber
+            // 
+            this.GuessNumber.Location = new System.Drawing.Point(12, 42);
+            this.GuessNumber.Name = "GuessNumber";
+            this.GuessNumber.Size = new System.Drawing.Size(100, 22);
+            this.GuessNumber.TabIndex = 0;
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Location = new System.Drawing.Point(118, 42);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(75, 23);
+            this.GuessButton.TabIndex = 1;
+            this.GuessButton.Text = "猜數字";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
+            // 
+            // NewNumber
+            // 
+            this.NewNumber.Location = new System.Drawing.Point(13, 13);
+            this.NewNumber.Name = "NewNumber";
+            this.NewNumber.Size = new System.Drawing.Size(75, 23);
+            this.NewNumber.TabIndex = 2;
+            this.NewNumber.Text = "產生新數字";
+            this.NewNumber.UseVisualStyleBackColor = true;
+            this.NewNumber.Click += new System.EventHandler(this.NewNumber_Click);
+            // 
+            // AnswerLabel
+            // 
+            this.AnswerLabel.AutoSize = true;
+            this.AnswerLabel.Font = new System.Drawing.Font("新細明體", 10F);
+            this.AnswerLabel.Location = new System.Drawing.Point(175, 18);
+            this.AnswerLabel.Name = "AnswerLabel";
+            this.AnswerLabel.Size = new System.Drawing.Size(0, 14);
+            this.AnswerLabel.TabIndex = 3;
+            // 
+            // AnswerButton
+            // 
+            this.AnswerButton.Location = new System.Drawing.Point(94, 13);
+            this.AnswerButton.Name = "AnswerButton";
+            this.AnswerButton.Size = new System.Drawing.Size(75, 23);
+            this.AnswerButton.TabIndex = 4;
+            this.AnswerButton.Text = "公布答案";
+            this.AnswerButton.UseVisualStyleBackColor = true;
+            this.AnswerButton.Click += new System.EventHandler(this.AnswerButton_Click);
+            // 
+            // ShowBox
+            // 
+            this.ShowBox.FormattingEnabled = true;
+            this.ShowBox.ItemHeight = 12;
+            this.ShowBox.Location = new System.Drawing.Point(13, 71);
+            this.ShowBox.Name = "ShowBox";
+            this.ShowBox.Size = new System.Drawing.Size(250, 244);
+            this.ShowBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 331);
+            this.Controls.Add(this.ShowBox);
+            this.Controls.Add(this.AnswerButton);
+            this.Controls.Add(this.AnswerLabel);
+            this.Controls.Add(this.NewNumber);
+            this.Controls.Add(this.GuessButton);
+            this.Controls.Add(this.GuessNumber);
+            this.Name = "Form1";
+            this.Text = "猜數字";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox GuessNumber;
+        private System.Windows.Forms.Button GuessButton;
+        private System.Windows.Forms.Button NewNumber;
+        private System.Windows.Forms.Label AnswerLabel;
+        private System.Windows.Forms.Button AnswerButton;
+        private System.Windows.Forms.ListBox ShowBox;
+    }
+}
+
---
+++ 1A2B/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _1A2B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] ans = new int[4];
+        int count = 0;
+        private void NewNumber_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int i = 0;
+            while (i < 4)
+            {
+                ans[i] = rnd.Next(0, 9);
+                int j = 0;
+                while (j < i)
+                {
+                    if (ans[j] == ans[i])
+                    {
+                        i = i - 1;
+                        break;
+                    }
+                    j = j + 1;
+                }
+                i = i + 1;
+            }
+            AnswerLabel.Text = "";
+            count = 0;
+            GuessNumber.Text = "";
+            ShowBox.Items.Clear();
+        }
+
+        private void AnswerButton_Click(object sender, EventArgs e)
+        {
+            AnswerLabel.Text = "答案為：" + ans[0] + ans[1] + ans[2] + ans[3];
+        }
+
+        private void GuessButton_Click(object sender, EventArgs e)
+        {
+            int A = 0, B = 0;
+            int guess = int.Parse(GuessNumber.Text);
+            int[] guessArray = new int[4];
+            int i = 3;
+            while (i >= 0)
+            {
+                guessArray[i] = guess % 10;
+                i = i - 1;
+                guess = guess / 10;
+            }
+            i = 0;
+            while (i < 4)
+            {
+                int j = 0;
+                while (j < 4)
+                {
+                    if (ans[i] == guessArray[j])
+                    {
+                        if (i == j)
+                        {
+                            A = A + 1;
+                            break;
+                        }
+                        else
+                        {
+                            B = B + 1;
+                            break;
+                        }
+                    }
+                    j = j + 1;
+                }
+                i = i + 1;
+            }
+            count = count + 1;
+            ShowBox.Items.Add("第" + count + "次：" + GuessNumber.Text + "，" + A + "A" + B + "B");
+            if (A == 4)
+            {
+                MessageBox.Show("你贏了!");
+            }
+        }
+    }
+}
